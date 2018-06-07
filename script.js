511e2a1d481cef250396633dea117a59b93269b1
morgan979 <morgan979@gmail.com>
2018-06-06 23:54:23 -0500

    sliding window and modals for images

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = function () {
+	// Modal
 	// Get the modal
 	// var modal = document.getElementById('myModal');
 	// console.log(modal);
@@ -18,41 +18,78 @@ window.onload = function () {
 	// var span = document.getElementsByClassName("close")[0];
 
 	// // When the user clicks on <span> (x), close the modal
-	// span.onclick = function() { 
-	//   modal.style.display = "none";
-	// }
+	// // window.onclick = function(event) { 
+	// //   modal.style.display = "none";
+	// // }
+	// modal.addEventListener('click',function(){
+	// this.style.display="none";
+	// })
+
+	   //  function lightbox(idx) {
+    //     //Show the slider wrapper
+    //     var ninjaSldr = document.getElementById("ninja-slider");
+    //     ninjaSldr.parentNode.style.display = "block";
+
+    //     //Then init the slider
+    //     //Note: The { initSliderByCallingInitFunc: true } option in the 
+    //     // ninja-slider.js tells the page not to initiate the slider
+    //     // unless the following init(idx) function is called.
+    //     // nslider.init(idx);
+
+    //     //Then mimic clicking the fullscreen button to popup the modal
+    //     var fsBtn = document.getElementById("fsBtn");
+    //     fsBtn.click();
+    // }
+
+    // function fsIconClick(isFullscreen, ninjaSldr) {
+    //     //Note: fsIconClick is the default event handler of the fullscreen button
+    //     if (isFullscreen) {
+    //         ninjaSldr.parentNode.style.display = "none";
+    //     }
+    // }
 
 
+// Open the Modal
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+}
 
+// Close the Modal
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+}
 
+// var modal = document.getElementById('myModal');
+// 	modal.addEventListener('click',function(){
+// 	this.style.display="none";
+// 	})
 
+//Slideshow
+var slideIndex = 1;
+showSlides(slideIndex);
 
-	var slideIndex = 1;
-	showSlides(slideIndex);
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-	// Next/previous controls
-	function plusSlides(n) {
-	  showSlides(slideIndex += n);
-	}
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-	// Thumbnail image controls
-	function currentSlide(n) {
-	  showSlides(slideIndex = n);
-	}
-
-	function showSlides(n) {
-	  var i;
-	  var slides = document.getElementsByClassName("mySlides");
-	  var dots = document.getElementsByClassName("dot");
-	  if (n > slides.length) {slideIndex = 1} 
-	  if (n < 1) {slideIndex = slides.length}
-	  for (i = 0; i < slides.length; i++) {
-	      slides[i].style.display = "none"; 
-	  }
-	  for (i = 0; i < dots.length; i++) {
-	      dots[i].className = dots[i].className.replace(" active", "");
-	  }
-	  slides[slideIndex-1].style.display = "block"; 
-	  dots[slideIndex-1].className += " active";
-	}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
 }
